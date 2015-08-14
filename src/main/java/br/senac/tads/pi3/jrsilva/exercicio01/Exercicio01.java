151d0dfa4c1e69937dc20bea29319c853559ab5d
JOSE ROBERTO <JOSE ROBERTO@JOSEROBERTO-PC.senacsp.edu.br>
2015-08-14 20:06:17 -0300

    Inclusao do metodo man

---
+++ src/main/java/br/senac/tads/pi3/jrsilva/exercicio01/Exercicio01.java
@@ -0,0 +1,14 @@
+
+
+package br.senac.tads.pi3.jrsilva.exercicio01;
+
+
+public class Exercicio01 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+    
+    
+}
