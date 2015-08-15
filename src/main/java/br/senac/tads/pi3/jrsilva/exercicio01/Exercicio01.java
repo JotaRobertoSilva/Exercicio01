fb2a5ee4833fa6c7d7f2223dd22930f1384024f0
JOSE ROBERTO <JOSE ROBERTO@JOSEROBERTO-PC.senacsp.edu.br>
2015-08-14 21:14:36 -0300

    Inclusao do metodo man2

--- src/main/java/br/senac/tads/pi3/jrsilva/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/jrsilva/exercicio01/Exercicio01.java
@@ -6,7 +6,7 @@ package br.senac.tads.pi3.jrsilva.exercicio01;
 public class Exercicio01 {
     
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World-teste");
     }
     
     
